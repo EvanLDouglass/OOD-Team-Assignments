b35c81c8fdcb02f57063516e11724f6705a162d5
Evan <evandoug90@gmail.com>
2019-07-14 15:48:38 -0700

    Update formatting

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IMediaLibrary.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IMediaLibrary.java
@@ -11,6 +11,7 @@ public interface IMediaLibrary {
 
   /**
    * Adds an {@code IFilmMedia} object to the the media library.
+   *
    * @param media the media to add, either a Movie or TVShow
    * @throws AliasAlreadyExistsException if the alias given to the media is already used in this
    *     library
@@ -20,6 +21,7 @@ public interface IMediaLibrary {
   /**
    * Given a director's name, returns a list of the {@code IFilmMedia} objects that were directed by
    * that director.
+   *
    * @param directorName the Name of a director to search for
    * @return a list of the movies and tv shows that were directed by the given director
    */
@@ -27,6 +29,7 @@ public interface IMediaLibrary {
 
   /**
    * Returns the most streamed {@code IFilmMedia} object in this media library.
+   *
    * @return the most streamed movie or tv show in this media library
    */
   IFilmMedia getMostStreamed();
@@ -34,6 +37,7 @@ public interface IMediaLibrary {
   /**
    * Given an alias for an {@code IFIlmMedia}, returns the number of times that media has been
    * streamed from this media library.
+   *
    * @param alias the alias for a movie or tv show
    * @return the number of times that the given media or tv show has been streamed
    */
@@ -42,6 +46,7 @@ public interface IMediaLibrary {
   /**
    * Streams an {@code IFilmMedia} object. At this time, {@code streamMedia} only increments the
    * recorded number of times that something was streamed.
+   *
    * @param alias the alias for a movie or tv show to stream
    */
   void streamMedia(String alias);
