525f556abffe0f59f1745623c20f6d06b7a4c1d2
Evan <evandoug90@gmail.com>
2019-07-13 22:58:08 -0700

    Continue FilmProfessional implementation

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/AbstractFilmProfessional.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/AbstractFilmProfessional.java
@@ -1,13 +1,43 @@
 package edu.neu.khoury.cs5004.assignment7.problem1;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Implements the {@code IFilmProfessional} interface.
+ *
+ * @author evandouglass
+ */
 public abstract class AbstractFilmProfessional implements IFilmProfessional {
 
   private Name name;
   private List<Movie> movies;
   private List<TVSeries> series;
+
+  /**
+   * Constructor for an {@code AbstractFilmProfessional}.
+   *
+   * @param name the name of this professional
+   * @param movies a list of movies they have worked on
+   * @param series a list of tv series they have worked on
+   */
+  public AbstractFilmProfessional(Name name,
+      List<Movie> movies, List<TVSeries> series) {
+    this.name = name;
+    this.movies = movies;
+    this.series = series;
+  }
+
+  /**
+   * Basic constructor for {@code AbstractFilmProfessional} with only a name parameter. Initializes
+   * the movies and tv series as empty {@code ArrayList}s.
+   *
+   * @param name the name of this professional
+   */
+  public AbstractFilmProfessional(Name name) {
+    this(name, new ArrayList<>(), new ArrayList<>());
+  }
 
   /* Methods */
 
@@ -21,14 +51,29 @@ public abstract class AbstractFilmProfessional implements IFilmProfessional {
     addTVSeries(series);
   }
 
-  @Override
-  public void addMovie(Movie movie) {}
+  /**
+   * Adds a movie to the list of movies that this this person has worked on.
+   *
+   * @param movie the movie to add
+   */
+  public void addMovie(Movie movie) {
+    this.movies.add(movie);
+  }
 
-  @Override
-  public void addTVSeries(TVSeries series) {}
+  /**
+   * Adds a TV series to the list of series that this person has worked on.
+   *
+   * @param series the TV series to add
+   */
+  public void addTVSeries(TVSeries series) {
+    this.series.add(series);
+  }
+
+  /* Getters */
 
   @Override
   public List<IFilmMedia> getMedia() {
+    // TODO: add together movie and tv lists, sort before return
     return null;
   }
 
@@ -39,20 +84,24 @@ public abstract class AbstractFilmProfessional implements IFilmProfessional {
 
   @Override
   public List<Movie> getMovies() {
+    // TODO: sort by year
     return movies;
   }
 
   @Override
   public List<TVSeries> getTVSeries() {
+    // TODO: sort by year
     return series;
   }
 
+  /* Comparator classes */
+
   /**
-   * A simple class used to compare two {@code IFilmMedia} objects by their year of release.
-   * Note that {@code IFilmMedia} objects are not {@code Comparable}. This is because there is
-   * no clear way to order these objects in a way that will be consistent with {@code equals}.
-   * It is more appropriate to instead make a {@code Comparator} class in order to compare two of
-   * these objects by individual fields.
+   * A simple class used to compare two {@code IFilmMedia} objects by their year of release. Note
+   * that {@code IFilmMedia} objects are not {@code Comparable}. This is because there is no clear
+   * way to order these objects in a way that will be consistent with {@code equals}. It is more
+   * appropriate to instead make a {@code Comparator} class in order to compare two of these objects
+   * by individual fields.
    */
   private class CompareByYear implements Comparator<IFilmMedia> {
 
--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IFilmProfessional.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IFilmProfessional.java
@@ -2,21 +2,54 @@ package edu.neu.khoury.cs5004.assignment7.problem1;
 
 import java.util.List;
 
+/**
+ * Describes the basic expected functionality of someone that works on film projects (i.e. movies, tv series)
+ * for a living.
+ */
 public interface IFilmProfessional {
 
-  void addMovie(Movie movie);
-
-  void addTVSeries(TVSeries series);
-
+  /**
+   * Adds a {@code Movie} to this professional's resume.
+   *
+   * @param movie the movie that this professional worked on
+   */
   void addMedia(Movie movie);
 
+  /**
+   * Adds a {@code TVSeries} to this professional's resume.
+   *
+   * @param series the TV series that this professional worked on
+   */
   void addMedia(TVSeries series);
 
+  /**
+   * Returns the name of this film professional.
+   *
+   * @return the name of this film professional
+   */
   Name getName();
 
+  /**
+   * Returns a list of the {@code Movie}s on this professional's resume, ordered by most recent to
+   * oldest.
+   *
+   * @return an ordered list of Movies, by year of release
+   */
   List<Movie> getMovies();
 
+  /**
+   * Returns a list of the {@code TVSeries}s on this professional's resume, ordered by most recent
+   * to oldest.
+   *
+   * @return an ordered list of TVSeries, by year of release
+   */
   List<TVSeries> getTVSeries();
 
+  /**
+   * Returns a list of the {@code Movie}s and {@code TVSeries} on this professional's resume,
+   * ordered by most recent to oldest.
+   *
+   * @return an ordered list of movies and series, by year of release
+   */
   List<IFilmMedia> getMedia();
 }
