051ca22f9413a142fb515333783af9829ea4b139
Evan <evandoug90@gmail.com>
2019-07-13 23:07:01 -0700

    Implement Director class

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/Director.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/Director.java
@@ -1,5 +1,35 @@
 package edu.neu.khoury.cs5004.assignment7.problem1;
 
+import java.util.List;
+
+/**
+ * Represents a film director. Includes fields for their name and lists of movies and tv series that
+ * they have directed.
+ *
+ * @author evandouglass
+ */
 public class Director extends AbstractFilmProfessional {
 
+  /**
+   * Constructor for an {@code Director}.
+   *
+   * @param name the name of this director
+   * @param movies a list of movies they have worked on
+   * @param series a list of tv series they have worked on
+   */
+  public Director(Name name,
+      List<Movie> movies,
+      List<TVSeries> series) {
+    super(name, movies, series);
+  }
+
+  /**
+   * Basic constructor for {@code Director} with only a name parameter. Initializes
+   * the movies and tv series as empty {@code ArrayList}s.
+   *
+   * @param name the name of this professional
+   */
+  public Director(Name name) {
+    super(name);
+  }
 }
