eb5464508dbea29bb8d750cafd40ce8e453bc912
Evan <evandoug90@gmail.com>
2019-07-14 12:44:41 -0700

    Add to media library ADT

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IMediaLibrary.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/IMediaLibrary.java
@@ -1,5 +1,19 @@
 package edu.neu.khoury.cs5004.assignment7.problem1;
 
+/**
+ * Describes the functionality of a media library of movies and tv series.
+ *
+ * @author evandouglass
+ */
 public interface IMediaLibrary {
 
+  void add(IFilmMedia media) throws AliasAlreadyExistsException;
+
+  void add(Movie movie) throws AliasAlreadyExistsException;
+
+  void add(TVSeries series) throws AliasAlreadyExistsException;
+
+  IFilmMedia getMediaFrom(Name directorName);
+
+  Integer getTimesStreamed(String alias);
 }
