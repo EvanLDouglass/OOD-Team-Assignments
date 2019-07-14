c1b67664a5eac4cf8a54cef2e6c74b1a2c497e08
Evan <evandoug90@gmail.com>
2019-07-13 19:33:08 -0700

    Add fields

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/AbstractFilmProfessional.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/AbstractFilmProfessional.java
@@ -1,6 +1,10 @@
 package edu.neu.khoury.cs5004.assignment7.problem1;
 
-public class AbstractFilmProfessional implements IFilmProfessional {
+import java.util.List;
+
+public abstract class AbstractFilmProfessional implements IFilmProfessional {
 
   private Name name;
+  private List<Movie> movies;
+  private List<TVSeries> series;
 }
