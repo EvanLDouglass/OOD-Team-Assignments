31854f71d1fa326c66546c237594e971acfbb3d3
Evan <evandoug90@gmail.com>
2019-07-13 19:30:35 -0700

    Add class TVSeries

---
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/TVSeries.java
@@ -0,0 +1,22 @@
+package edu.neu.khoury.cs5004.assignment7.problem1;
+
+import java.util.List;
+
+public class TVSeries extends AbstractFilmMedia {
+
+  /**
+   * Main constructor for a {@code TVSeries}.
+   *
+   * @param alias an alias/nickname for the TVSeries
+   * @param title the TVSeries's title
+   * @param yearOfRelease the TVSeries's year of release
+   * @param directors a list of the directors who worked on the TVSeries
+   * @param mainActors a list of main actors who worked on the TVSeries
+   * @throws NullObjectException if any of the parameters are null
+   */
+  public TVSeries(String alias, String title, Integer yearOfRelease,
+      List<IDirector> directors,
+      List<IActor> mainActors) throws NullObjectException {
+    super(alias, title, yearOfRelease, directors, mainActors);
+  }
+}
