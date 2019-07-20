3363e96daf31150c612aac564edee0be612abb51
Evan <evandoug90@gmail.com>
2019-07-20 14:56:00 -0700

    Rename file from TVSeries

--- assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/TvSeries.java
+++ assignment7/src/main/java/edu/neu/khoury/cs5004/assignment7/problem1/TvSeries.java
@@ -10,19 +10,19 @@ import java.util.List;
  *
  * @author evandouglass
  */
-public class TVSeries extends AbstractFilmMedia {
+public class TvSeries extends AbstractFilmMedia {
 
   /**
-   * Main constructor for a {@code TVSeries}.
+   * Main constructor for a {@code TvSeries}.
    *
-   * @param alias an alias/nickname for the TVSeries
-   * @param title the TVSeries's title
-   * @param yearOfRelease the TVSeries's year of release
-   * @param directors a list of the directors who worked on the TVSeries
-   * @param mainActors a list of main actors who worked on the TVSeries
+   * @param alias an alias/nickname for the TvSeries
+   * @param title the TvSeries's title
+   * @param yearOfRelease the TvSeries's year of release
+   * @param directors a list of the directors who worked on the TvSeries
+   * @param mainActors a list of main actors who worked on the TvSeries
    * @throws NullObjectException if any of the parameters are null
    */
-  public TVSeries(String alias, String title, Integer yearOfRelease,
+  public TvSeries(String alias, String title, Integer yearOfRelease,
       List<Name> directors,
       List<Name> mainActors) throws NullObjectException, InvalidArgumentException {
     super(alias, title, yearOfRelease, directors, mainActors);
@@ -32,6 +32,6 @@ public class TVSeries extends AbstractFilmMedia {
 
   @Override
   public String toString() {
-    return "TVSeries{" + super.toString() + "}";
+    return "TvSeries{" + super.toString() + "}";
   }
 }
