172047e2f31362ffb5d825281d28548cf71154e5
Evan <evandoug90@gmail.com>
2019-08-05 18:45:14 -0700

    Rename main class

--- assignment9/src/main/java/edu/neu/khoury/cs5004/assignment9/SecureBankVerificationSimulator.java
+++ assignment9/src/main/java/edu/neu/khoury/cs5004/assignment9/SecureBankVerificationSimulator.java
@@ -1,13 +1,9 @@
 package edu.neu.khoury.cs5004.assignment9;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class SecureBankVerificationSimulator
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
