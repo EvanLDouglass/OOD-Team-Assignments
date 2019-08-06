fc035d189cdfc229d05ee1a75395871854da8dd6
Evan <evandoug90@gmail.com>
2019-08-05 18:42:22 -0700

    Initial commit

---
+++ assignment9/src/main/java/edu/neu/khoury/cs5004/assignment9/App.java
@@ -0,0 +1,13 @@
+package edu.neu.khoury.cs5004.assignment9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
