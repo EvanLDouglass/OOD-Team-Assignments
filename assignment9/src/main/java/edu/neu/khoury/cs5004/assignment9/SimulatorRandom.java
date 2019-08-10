05f52c06e3eb33b9c92b5076401c53f20206baad
Evan <evandoug90@gmail.com>
2019-08-10 13:41:39 -0700

    Change name and add different random generators

---
+++ assignment9/src/main/java/edu/neu/khoury/cs5004/assignment9/SimulatorRandom.java
@@ -0,0 +1,32 @@
+package edu.neu.khoury.cs5004.assignment9;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Generates random messages for the secure bank simulator.
+ */
+public class SimulatorRandom {
+  private static final int MAX_MSG = 30001;
+  private static final int MAX_WITHDRAW = 3001;
+  private static final int MAX_DEPOSIT = 2001;
+
+  private static Random random = new SecureRandom();
+
+  /**
+   * Generate message integer.
+   *
+   * @return the integer
+   */
+  public static Integer generateMessage(){
+    return random.nextInt(MAX_MSG);
+  }
+
+  public static Integer generateWithdrawLimit() {
+    return random.nextInt(MAX_WITHDRAW);
+  }
+
+  public static Integer generateDepositLimit() {
+    return random.nextInt(MAX_DEPOSIT);
+  }
+}
---
+++ assignment9/src/test/java/edu/neu/khoury/cs5004/assignment9/SimulatorRandomTest.java
@@ -0,0 +1,38 @@
+package edu.neu.khoury.cs5004.assignment9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimulatorRandomTest {
+  private static final int MAX_MSG = 30000;
+  private static final int MAX_WITHDRAW = 3000;
+  private static final int MAX_DEPOSIT = 2000;
+
+  @Test
+  public void generateMessage() {
+    for (int i = 0; i < 1000; i++) {
+      Integer msg = SimulatorRandom.generateMessage();
+      assertTrue(msg <= (Integer) MAX_MSG);
+      assertTrue(msg >= (Integer) 0);
+    }
+  }
+
+  @Test
+  public void generateWithdrawLimit() {
+    for (int i = 0; i < 1000; i++) {
+      Integer limit = SimulatorRandom.generateWithdrawLimit();
+      assertTrue(limit <= (Integer) MAX_WITHDRAW);
+      assertTrue(limit >= (Integer) 0);
+    }
+  }
+
+  @Test
+  public void generateDepositLimit() {
+    for (int i = 0; i < 1000; i++) {
+      Integer limit = SimulatorRandom.generateDepositLimit();
+      assertTrue(limit <= (Integer) MAX_DEPOSIT);
+      assertTrue(limit >= (Integer) 0);
+    }
+  }
+}
